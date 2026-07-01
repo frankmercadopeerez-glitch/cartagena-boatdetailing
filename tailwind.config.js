3381cb5be8acc4ec09967c337bec5574444bf936
FRANK MERCADO <frankmercadopeerez@gmail.com>
2026-06-30 22:56:29 -0500

    feat: add contratos section with CBD-PS-001-2026 (Yovanny Guzman / M/Y Leyla)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./blog/**/*.html", "./cotizaciones/*.html"],
+  content: ["./*.html", "./blog/**/*.html", "./cotizaciones/*.html", "./contratos/*.html"],
   theme: {
     extend: {
       fontFamily: {
