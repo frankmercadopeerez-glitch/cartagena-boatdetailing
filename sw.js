c660eb0e7cf74847d98fc0702ba6b56fa134b8e9
FRANK MERCADO <frankmercadopeerez@gmail.com>
2026-07-17 14:39:23 -0500

    Prevent zero partner balances during engine cache updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_STATIC = "cbd-static-v2";
 const CACHE_PAGES = "cbd-pages-v1";
-const FINANZAS_CACHE = "cbd-finanzas-v25";
+const FINANZAS_CACHE = "cbd-finanzas-v26";
 
 // Archivos del shell estático (CSS, JS, fuentes, imágenes críticas)
 const STATIC_SHELL = [
