8b6e22f7e27b5ebfe5e84b0034a10ed7cd5f6505
FRANK MERCADO <frankmercadopeerez@gmail.com>
2026-07-17 23:30:12 -0500

    Keep Orchid open for manual final close

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_STATIC = "cbd-static-v2";
 const CACHE_PAGES = "cbd-pages-v1";
-const FINANZAS_CACHE = "cbd-finanzas-v29";
+const FINANZAS_CACHE = "cbd-finanzas-v30";
 
 // Archivos del shell estático (CSS, JS, fuentes, imágenes críticas)
 const STATIC_SHELL = [
