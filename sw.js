7f3e0de012809ef4b228266d55c009fc70e6ad1e
FRANK MERCADO <frankmercadopeerez@gmail.com>
2026-08-09 01:55:48 -0500

    fix: restore mobile finance app scale

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_STATIC = "cbd-static-v3";
 const CACHE_PAGES = "cbd-pages-v2";
-const FINANZAS_CACHE = "cbd-finanzas-v37";
+const FINANZAS_CACHE = "cbd-finanzas-v38";
 
 // Archivos del shell estático (CSS, JS, fuentes, imágenes críticas)
 const STATIC_SHELL = [
